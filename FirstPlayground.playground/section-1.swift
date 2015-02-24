ee729a5847593fab1da8cfac9ffca71f61b51a67
edzye101 <edzye101@gmail.com>
2015-02-24 15:38:06 -0800

    More swift playgrounds

--- FirstPlayground.playground/section-1.swift
+++ FirstPlayground.playground/section-1.swift
@@ -7,11 +7,17 @@ var x = 0
 
 let label = UILabel(frame: CGRect(x: 0, y: 0, width:100, height: 30))
 
+
+
 label.backgroundColor = UIColor.greenColor()
+
+var a = []
 
 for i in 1...100 {
     x = i * i
     println(x)
+    
     label.text = "x is now \(x)"
     label.sizeToFit()
 }
+
---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str : String = "Hello, "
+
+str = "Hello, "
+
+var programmingLanguage = "Swift"
+
+let modernProgrammingLanguage = "Objective-Co"
+// Defining a camel case variable 
+
+
+var greeting = " \(str) \(modernProgrammingLanguage)"
+
+var gondola = 40
+
+gondola * 4
+
+
+print("To build an iPhone app we need to learn \(modernProgrammingLanguage)")
+
+
