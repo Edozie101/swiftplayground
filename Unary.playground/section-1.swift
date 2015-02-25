417a8554c02556ea22a27bd1e74a3c435bf19912
edzye101 <edzye101@gmail.com>
2015-02-25 12:23:35 -0800

    added unary playgound

--- Unary.playground/section-1.swift
+++ Unary.playground/section-1.swift
@@ -4,3 +4,30 @@ import UIKit
 
 var levelScore = 0
 
+levelScore--
+levelScore
+
+var totalScore = --levelScore
+
+totalScore
+levelScore
+
+// ++ increment unary operator 
+// -- decrement unary operator
+// -  negative operator
+
+levelScore = -levelScore
+
+// !NOT Operator
+
+let on = false
+let off = !on
+
+func fahrenheitToCelcius (var number : Double) {
+    (number - 32) / 1.8
+    
+
+}
+
+
+fahrenheitToCelcius(0)
