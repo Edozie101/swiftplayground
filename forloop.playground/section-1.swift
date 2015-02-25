67cf5a546025429aba5daf8b97db05386bf7c906
edzye101 <edzye101@gmail.com>
2015-02-25 13:51:44 -0800

    added forinloop playgound

---
+++ forloop.playground/section-1.swift
@@ -0,0 +1,27 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var todo : [String] = ["Return Calls", "Book Concert Tickets", "Pick up laundry"]
+
+println(todo[0])
+println(todo[1])
+println(todo[2])
+
+for item in todo {
+    println(item)
+}
+
+for number in 1...100 {
+    println("\(number) times 2 is \(number * 2 )")
+    
+
+}
+
+for number in 1...10 {
+    println("\(number) * 7 = \(number * 7 )")
+
+}
