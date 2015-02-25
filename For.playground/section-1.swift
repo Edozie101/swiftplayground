8e55ad795f3bb63a5c54cb5f98d742542428ae89
edzye101 <edzye101@gmail.com>
2015-02-25 14:08:44 -0800

    finished forloop playgound

--- For.playground/section-1.swift
+++ For.playground/section-1.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+var todo : [String] = ["Google", "Facebook", "Nvidia","Razer","Apple"]
+
+var index = 0 // index
+
+while index < todo.count { // condition
+    println(todo[index])
+    index++ // increment
+    
+}
+
+for var i = 0; i < todo.count ; i++ {
+
+    println(todo[i])
+}
+
+
+for var index = 0; index < todo.count; index++ {
+    
+    println( "\(todo[index])   I gotta start this")
+    }
+
