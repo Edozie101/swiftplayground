a0b8fd507a98ea58ba01de79c727f12842eaadfc
edzye101 <edzye101@gmail.com>
2015-02-25 14:55:27 -0800

    finished Conditional  playgound

--- Comparison.playground/section-1.swift
+++ Comparison.playground/section-1.swift
@@ -5,13 +5,42 @@ import UIKit
 1 == 1 // true, because 1 is equal to 1 
 2 != 1 // t
 
-let distance = 5 // in miles
+let distance = 100 // in miles
 if distance < 5 {
 
     println("\(distance) miles is near")
-} else if distance >= 5 {
+} else if distance >= 5 && distance <= 20 {
     println("\(distance) miles is close")
 } else {
     println("\(distance) miles is far")
+
+}
+
+if distance >= 5 && distance < 10 {
+    println("AND")
+    
+} else if distance == 5 || distance < 7 {
+    println("its less than 7 too")
+}
+
+
+var numbers = 1...20
+
+for number in numbers {
+
+    if  (number % 5 == 0) && (number % 3 == 0) {
+        println("\(number) is Fizzbuzz")
+    }
+        
+    else if number % 3 == 0 {
+        println("\(number) is Fizz")
+    }
+
+        
+    else if (number % 5 == 0) {
+      println("\(number) is Buzz")
+    }
+    
+  
 
 }
