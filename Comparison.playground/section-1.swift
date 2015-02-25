267399075dc7007b50e067fe1669bea7841ad8f6
edzye101 <edzye101@gmail.com>
2015-02-25 14:32:57 -0800

    added Comparison playgound

---
+++ Comparison.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+1 == 1 // true, because 1 is equal to 1 
+2 != 1 // t
+
+let distance = 5 // in miles
+if distance < 5 {
+
+    println("\(distance) miles is near")
+} else if distance >= 5 {
+    println("\(distance) miles is close")
+} else {
+    println("\(distance) miles is far")
+
+}
