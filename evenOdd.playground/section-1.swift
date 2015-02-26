9ef325aa8c25afcc3161824b87c6fe401f0452f7
edzye101 <edzye101@gmail.com>
2015-02-25 18:13:44 -0800

    Added evenOdd exercise

---
+++ evenOdd.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var listNumbers = 1...100
+
+for number in listNumbers {
+    if number % 2 == 0 {
+    
+    println("\(number) is even" )
+    
+    }
+    else{
+    println("\(number) is odd")
+    
+    }
+
+}
+
+
