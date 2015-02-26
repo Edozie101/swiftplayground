10471c15c3f20aa39e7197599ee22b3d6070274f
edzye101 <edzye101@gmail.com>
2015-02-26 12:04:33 -0800

    added isDivisable playground

---
+++ isDivisable.playground/section-1.swift
@@ -0,0 +1,23 @@
+// is divisable
+
+import UIKit
+
+
+func isDivisable(#firstNumber: Int, #secondNumber: Int) -> (result : Bool?, description: String) {
+    
+    let division = firstNumber % secondNumber
+    
+    switch division{
+    case 0 :
+        println("\(firstNumber) is divisable by \(secondNumber)")
+        return (true,"\(firstNumber) is divisable by \(secondNumber)")
+    default :
+        println("\(firstNumber) is not diviable by \(secondNumber)")
+        return (false,"\(firstNumber) is not divisable by \(secondNumber)")
+    }
+
+
+}
+
+
+isDivisable(firstNumber: 2, secondNumber: 3 )
