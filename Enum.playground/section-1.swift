b561285950e440d4f85186f1477918f2576cc8a3
edzye101 <edzye101@gmail.com>
2015-03-01 21:51:02 -0800

    Enum swift playgrounds

--- Enum.playground/section-1.swift
+++ Enum.playground/section-1.swift
@@ -3,3 +3,30 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+func IsDivisable ( #Divisor: Int, #Dividend: Int ) -> Bool? {
+
+    
+    
+    if Divisor % Dividend == 0 {
+        println("\(Divisor) is Divisable by \(Dividend)")
+        return true
+    }
+    else    {
+        return false
+    
+    }
+
+}
+
+
+var companies : [String] = []
+
+companies.append("Google")
+
+companies.append("Twitter")
+
+
+
+companies.insert("Youtube", atIndex: 2)
