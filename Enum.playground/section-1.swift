bc74d4f5c818f2e788b7e8dd5f8e097f6819a14b
edzye101 <edzye101@gmail.com>
2015-03-02 18:23:14 -0800

    Enum swift playgrounds

--- Enum.playground/section-1.swift
+++ Enum.playground/section-1.swift
@@ -4,15 +4,28 @@ import UIKit
 var str = "Hello, playground"
 
 
+enum Day {
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+    case Sunday
+    
+}
+
+
+
 let days = ["Monday","Tuedays" , "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-func weekdayOrWeekend(dayOfweek: String) -> String{
+func weekdayOrWeekend(dayOfweek: Day) -> String{
 
     switch dayOfweek{
-        case "Monday","Tuedays" , "Wednesday", "Thursday", "Friday":
-            return "Its a weekday"
+    case Day.Monday, Day.Tuesday, Day.Wednesday, Day.Thursday, Day.Friday:
+        return "Its a weekday"
 
-        case "Saturday", "Sunday":
+    case Day.Saturday, Day.Sunday :
             return "YAY Its the weekend"
      default:
         return "Not a valid day"
@@ -20,7 +33,50 @@ func weekdayOrWeekend(dayOfweek: String) -> String{
 
 }
 
-weekdayOrWeekend(days[6])
+weekdayOrWeekend(Day.Sunday)
 
 
-enum
+
+
+enum Computer {
+    case Laptop, Desktop, Mobile_Phone
+
+}
+
+
+func isThisaGoodComputer ( typeOfComputer : Computer) -> (Bool?,String) {
+    
+    switch typeOfComputer {
+    
+    case .Mobile_Phone :
+            return (true, "This is dope as fuck")
+    
+    default :
+            return(false, "This isnt a Computer")
+    
+    }
+
+}
+
+
+
+
+var crazyIdea : Bool? = false
+
+
+
+if let result = crazyIdea {
+
+    println("I returned True I am unwrapped")
+
+}
+
+
+enum Character {
+    case a, b , c, d, e, f, g, h,i ,j ,k
+
+}
+
+
+
+C
